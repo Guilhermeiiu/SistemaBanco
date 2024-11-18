e9283d3ad806c6ed8e077d5038c9727bc138dabe
Guilhermeiiu <90458302+Guilhermeiiu@users.noreply.github.com>
2024-11-18 20:08:28 -0300

    Add files via upload

---
+++ AtividadeVA/template.php
@@ -0,0 +1,65 @@
+<html>
+    <head>
+    <meta charset="utf-8" />
+    <title>Gerenciador de Tarefas</title>
+    <link rel="stylesheet" href="tarefas.css" type="text/css" />
+    </head>
+    <body>
+        <h1>Gerenciamento de tarefas</h1>
+        <form>
+            <fieldset>
+                <legend>Nova tarefa</legend> 
+                    <label>
+                        Tarefa:
+                        <input type="text" name="nome"/>
+                    </label>
+                
+                    <label>
+                        Descrição (Opcional):
+                        <textarea name="descricao"></textarea>
+                    </label>
+                
+                <label>
+                    Prazo (Opcional):
+                    <input type="text" name="prazo" />
+                </label>
+
+                <fieldset>
+                    <legend>Prioridade:</legend>
+                    <label>
+                        <input type="radio" name="prioridade" value="baixa" checked />
+                        Baixa
+                        <input type="radio" name="prioridade" value="media" />
+                        Média
+                        <input type="radio" name="prioridade" value="alta" />
+                        Alta
+                    </label>
+                </fieldset>
+                <label>
+                Tarefa concluída:
+                <input type="checkbox" name="concluida" value="sim" />
+            </label>
+            <input type="submit" value="Cadastrar" />
+            </fieldset>
+        </form>
+        <table>
+            <tr>
+                <th>Tarefa</th>
+                <th>Descricao</th>
+                <th>Prazo</th>
+                <th>Prioridade</th>
+                <th>Concluída</th>
+            </tr>
+            <?php foreach ($lista_tarefas as $tarefas) :?>
+                <tr>
+                    <td><?php echo $tarefas['nome']; ?></td>
+                    <td><?php echo $tarefas['descricao']; ?></td>
+                    <td><?php echo $tarefas['prazo']; ?></td>
+                    <td><?php echo $tarefas['prioridade']; ?></td>
+                    <td><?php echo $tarefas['concluida']; ?></td>
+                </tr>
+            <?php endforeach; ?>
+        </table>
+    </body>
+    
+</html>
